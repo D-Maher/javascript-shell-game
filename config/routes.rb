ad880b55062970bcfc4913d0de34c7ab49c20ae2
Danny Maher <dmaher724@gmail.com>
2017-01-29 20:30:33 -0600

    Setup routes and controllers, migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'games#new'
+
+  resources :games, only: [:create, :new, :show]
+
+  resources :users, only: [:create, :new, :show]
+
+  resources :sessions, only: [:create, :new, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
